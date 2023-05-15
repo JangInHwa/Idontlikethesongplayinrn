8965bef9474d8ed10c08f9dd98b69b8e9d863f2e
uglyonlytoday <superjang3357@naver.com>
2023-05-15 20:16:00 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/uglyonlytoday/idontlikethesongplayinrn/i_dont_like_the_song_playin_rn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uglyonlytoday.idontlikethesongplayinrn.i_dont_like_the_song_playin_rn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
