943cbe31faacfde10ddd353815ad280e372574ce
uglyonlytoday <superjang3357@naver.com>
2023-05-16 21:13:12 +0900

    add supabase package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:i_dont_like_the_song_playin_rn/app/routes/pages.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://mgtmarhdqhaxjiviwpgc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ndG1hcmhkcWhheGppdml3cGdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxNDA4ODUsImV4cCI6MTk5OTcxNjg4NX0.xaxQEl_SImGW-FIzInac_6C6bx5Eg5lvk50-WFhO-_g',
+  );
+
   runApp(
     GetMaterialApp(
       initialRoute: Routes.DEV,
