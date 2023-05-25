6ea85cb07d885ab29f6047c0c0cf2f43d348f951
uglyonlytoday <superjang3357@naver.com>
2023-05-25 11:11:59 +0900

    feat : route to space after join space

--- lib/app/pages/join/controller.dart
+++ lib/app/pages/join/controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:i_dont_like_the_song_playin_rn/app/data/modules/space/controller.dart';
 import 'package:i_dont_like_the_song_playin_rn/app/data/modules/space/model.dart';
 import 'package:i_dont_like_the_song_playin_rn/app/data/modules/space/repository.dart';
+import 'package:i_dont_like_the_song_playin_rn/app/routes/pages.dart';
 
 class JoinPageController extends GetxController {
   TextEditingController textEditingController = TextEditingController();
@@ -25,22 +26,21 @@ class JoinPageController extends GetxController {
       return;
     }
 
-    final res = await SpaceRepository().getWithJoinCode(code);
+    resSpace.value = await SpaceRepository().getWithJoinCode(code);
 
-    if (res == null) {
+    if (resSpace.value == null) {
       noSpaceFoundMessageEnabled.value = true;
       return;
     }
-    if (spacesController.spaces.any((e) => e.id == res!.id)) {
+    if (spacesController.spaces.any((e) => e.id == resSpace.value!.id)) {
       alreadyJoinedMessageEnabled.value = true;
       return;
     }
-    resSpace.value = res;
     isCtaBtnEnabled.value = true;
   }
 
   Future<void> joinSpace(Space space) async {
     await spacesController.joinSpace(space);
-    Get.back();
+    Get.offNamed(Routes.SPACE, arguments: {'space': space});
   }
 }
